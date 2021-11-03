93a44e26f0bcf2e6a4f388a6fd15b23758c79f10
devlassev <vestergaardlasse2@gmail.com>
2021-11-03 21:17:13 +0100

    add image to page

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 var sound = new Audio("./assets/audio/rickroll.mp3");
 
 async function rickroll() {
+  // add rickroll sound to the page
   sound.currentTime = 0;
   sound.play();
+
+  // add text to the page
   document.getElementById("demo").innerHTML = "Never Gonna Give You Up";
+
+  // add image to the page
+  var img = document.createElement("img");
+  img.src = "./assets/img/nevergonnagiveyouup.gif";
+  img.height = "300";
+  img.width = "300";
+
+  document.body.appendChild(img);
 }
